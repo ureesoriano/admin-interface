3092047fa18500981b6355c7a763992236d4ab3f
Oriol Soriano <oriol.soriano@capside.com>
2018-12-31 10:45:31 +0000

    release 'admin-interface-mn-extensions' v0.0.1

---
+++ packages/extensions/dist/FieldType/Textarea/Textarea.js
@@ -0,0 +1,59 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _extends2 = require('babel-runtime/helpers/extends');
+
+var _extends3 = _interopRequireDefault(_extends2);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _jade = require('jade');
+
+var _jade2 = _interopRequireDefault(_jade);
+
+var _core = require('@admin-interface/core');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Textarea FieldType
+ * @extends FieldTypeAbstract
+ */
+class Textarea extends _core.FieldTypeAbstract {
+    constructor(...args) {
+        var _temp;
+
+        return _temp = super(...args), this._options = {
+            rows: 4
+        }, _temp;
+    }
+    /**
+     * @private
+     */
+
+
+    static getThisPath() {
+        return _path2.default.join(__dirname);
+    }
+
+    /**
+     * Render FieldType
+     * @param {{}} itemObject
+     * @returns {string}
+     */
+    render(itemObject) {
+        const value = itemObject ? this.getValueFromObject(itemObject) : this.getDefaultValue();
+        const context = (0, _extends3.default)({
+            value
+        }, this.context());
+
+        return _jade2.default.renderFile(this.constructor.getThisPathView(), context);
+    }
+}
+exports.default = Textarea;
+module.exports = exports['default'];
---
+++ packages/extensions/dist/Widget/ModelsInfoWidget/API.js
@@ -0,0 +1,48 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
+var _core = require('@admin-interface/core');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Controller
+ */
+const Controller = {
+    /**
+     * Get models information: list url and count
+     * @param {express$Request} req
+     * @param {express$Response} res
+     */
+    getModelsInfo(req, res) {
+        return (0, _asyncToGenerator3.default)(function* () {
+            const modelsKey = JSON.parse(req.query.models);
+            const promises = modelsKey.map((() => {
+                var _ref = (0, _asyncToGenerator3.default)(function* (modelKey) {
+                    const Model = _core.Registry.getRepository('Model').get(modelKey);
+                    if (Model) {
+                        const count = yield Model.getModel().count();
+                        return { count, model: modelKey };
+                    }
+                });
+
+                return function (_x) {
+                    return _ref.apply(this, arguments);
+                };
+            })());
+
+            Promise.all(promises).then(function (result) {
+                return res.json(result);
+            });
+        })();
+    }
+};
+exports.default = Controller;
+module.exports = exports['default'];
